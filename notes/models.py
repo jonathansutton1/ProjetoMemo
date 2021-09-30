aab4dc14b0ece310552b47fd33dda21915f7c9f5
Jonathan Sutton <jonathans@al.insper.edu.br>
2021-09-30 01:40:05 -0300

    Projeto MEMO funcionando

---
+++ notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return("{0}.{1}".format(self.id,self.title))
